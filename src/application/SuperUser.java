e1f8b1adb601af63adc5e097b4ba365b78fba855
Bhupesh S Kainth <43728552+bhupesh17040@users.noreply.github.com>
2018-12-01 17:14:58 +0530

    Updated

--- src/application/SuperUser.java
+++ src/application/SuperUser.java
@@ -8,6 +8,7 @@ public class SuperUser {
 	private final Database dbt;
 	private final String superuserID;
 	private final String password;
+
 	/**
 	 * @param dbt
 	 * @param superuserID
@@ -20,19 +21,21 @@ public class SuperUser {
 		this.password = password;
 	}
 	public void CreateWarehouse(String W_Name,String W_ID) {
-		 
+		 this.dbt.create_warehouse(W_Name,W_ID);
 	}
 	public void CreateStore(String S_Name,String S_ID) {
-		 
+		 this.dbt.create_store(S_Name,S_ID);
 	}
 	public void CreateWarehouseAdmin(String W_Admin_Name,String W_Admin_ID,String W_Admin_Password) {
-		
+		this.dbt.create_warehouseadmin(W_Admin_Name,W_Admin_ID,W_Admin_Password);
 	}
-	public void CreateStoreAdmin(String W_Admin_Name,String S_Admin_ID,String S_Admin_Password) {
-		 
+	public void CreateStoreAdmin(String S_Admin_Name,String S_Admin_ID,String S_Admin_Password) 
+	{
+		 this.dbt.create_storeadmin(S_Admin_Name,S_Admin_ID,S_Admin_Password);
 	}
-	public void LinkStoreWarehouse(String Store_Name,String Warehouse_name) {
-		
+	public void LinkStoreWarehouse(String Store_Name,String Warehouse_name) 
+	{
+		this.dbt.linkstorewarehouse(Store_Name,Warehouse_name);
 	}
 	/**
 	 * @return the dbt
@@ -55,3 +58,4 @@ public class SuperUser {
 	
 	
 }
+
